2e6bc38ca661f6b5367bf18b00109865108b773a
Nicolas Modrzyk <nico@karabiner-software.com>
2018-04-05 02:40:02 +0000

    commit tz setting

--- scripts/start-receive.sh
+++ scripts/start-receive.sh
@@ -1,1 +1,1 @@
-docker run -d -v `pwd`/db:/usr/src/app/db --restart=always --name absence-receive absence-receive
+docker run -d  -e TZ=Asia/Tokyo -v `pwd`/db:/usr/src/app/db --restart=always --name absence-receive absence-receive
