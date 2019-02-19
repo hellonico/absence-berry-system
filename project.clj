ce1fc8b25c9b227859ac3d47c7ceb5e38b44fbee
Nicolas Modrzyk <hellonico@gmail.com>
2019-02-19 15:36:09 +0900

    updating deps for lein 2.9

--- project.clj
+++ project.clj
@@ -3,18 +3,18 @@
   :jvm-opts ["-Duser.timezone=Asia/Tokyo -Dfile.encoding=UTF-8"]
   :ring {:handler absence.ringing/handler :init absence.ringing/init}
   :dependencies [
-  [org.clojure/clojure "1.8.0"]
+  [org.clojure/clojure "1.10.0"]
 
   [io.forward/clojure-mail "1.0.7"]
-  [com.draines/postal "2.0.2"]
-  [org.clojure/core.async "0.4.474"]
+  [com.draines/postal "2.0.3"]
+  [org.clojure/core.async "0.4.490"]
 
   [de.ubercode.clostache/clostache "1.4.0"]
-  [org.clojure/java.jdbc "0.7.5"]
-  [org.xerial/sqlite-jdbc "3.21.0.1"]
+  [org.clojure/java.jdbc "0.7.8"]
+  [org.xerial/sqlite-jdbc "3.25.2"]
 
-  [ring "1.6.3"]
-  [compojure "1.6.0"]
-  [yogthos/config "0.8"]
+  [ring "1.7.1"]
+  [compojure "1.6.1"]
+  [yogthos/config "1.1.1"]
 
   ])
