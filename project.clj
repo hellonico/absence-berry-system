dd034017fa5552d74cc3a195cf8edc3192d58375
Nicolas Modrzyk <hellonico@gmail.com>
2022-04-06 14:13:58 +0900

    Run lein ancient #8

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
   :ring {:handler absence.ringing/handler :init absence.ringing/init}
   :dependencies [[org.clojure/clojure "1.10.0"]
 
-                 [io.forward/clojure-mail "1.0.7"]
-                 [com.draines/postal "2.0.3"]
-                 [org.clojure/core.async "0.4.490"]
+                 [io.forward/clojure-mail "1.0.8"]
+                 [com.draines/postal "2.0.5"]
+                 [org.clojure/core.async "1.5.648"]
 
                  [de.ubercode.clostache/clostache "1.4.0"]
                   [org.clojure/java.jdbc "0.7.12"]
@@ -16,10 +16,9 @@
                  [org.xerial/sqlite-jdbc "3.36.0.3"]
 
                  [ring/ring "1.9.5"]
-                 [compojure "1.6.1"]
-                 [yogthos/config "1.1.1"]
+                 [compojure "1.6.2"]
+                 [yogthos/config "1.2.0"]
 
-  ; new excel
                  [org.clojars.mjdowney/excel-clj "2.1.0"]
                  [org.apache.logging.log4j/log4j-core "2.17.2"]
 
