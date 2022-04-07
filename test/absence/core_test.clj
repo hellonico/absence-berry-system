e0934262e4c72ed60d34a6eabcf0407cdeb7b317
Nicolas Modrzyk <hellonico@gmail.com>
2022-04-07 14:41:37 +0900

    move folders to dev-resources #16

--- test/absence/core_test.clj
+++ test/absence/core_test.clj
@@ -1,7 +1,6 @@
 (ns absence.core-test
   (:require [clojure.test :refer :all]
-            [absence.core :refer :all]))
+            [absence.utils :as u]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= java.time.YearMonth (class (u/to-yearmonth "2022-04-06")))))
