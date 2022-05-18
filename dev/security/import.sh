9398f21c67f772e7b57684ff772f7cc16f1123b5
Nicolas Modrzyk <hellonico@gmail.com>
2022-05-18 09:59:29 +0900

    update cert script

--- dev/security/import.sh
+++ dev/security/import.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 openssl x509 -outform der -in usgen-api-tw-otc.pem -out certificate.der
-keytool -storepass changeit -import -alias taka -keystore $JAVA_HOME/lib/security/cacerts -file certificate.der
-keytool -storepass changeit -keystore $JAVA_HOME/lib/security/cacerts -list | grep hello
+keytool -storepass changeit -import -alias mycert -keystore $JAVA_HOME/lib/security/cacerts -file certificate.der
+keytool -storepass changeit -keystore $JAVA_HOME/lib/security/cacerts -list | grep mycert
