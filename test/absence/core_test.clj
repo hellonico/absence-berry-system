c95159317ef6dd043fc2fe3a435ceee86f3f87e4
Nicolas Modrzyk <hellonico@gmail.com>
2022-10-24 14:38:05 +0900

    parse full dates ex: 0403 and 20220403 #128  ;  Setup for honeybadger #127

--- test/absence/core_test.clj
+++ test/absence/core_test.clj
@@ -4,10 +4,19 @@
             [absence.utils :as u]))
 
 (deftest a-test
-    (is (= java.time.YearMonth (class (u/to-yearmonth "2022-04-06")))))
+  (is (= java.time.YearMonth (class (u/to-yearmonth "2022-04-06")))))
 
 (deftest message-test
-  (are [x y] (= x y)
+  (are [x y]
+    (= x y)
+    (r/inner-drt "2022-03-10", "20220403,all day,vacation")
+    {:times "all day", :reason  "vacation", :holidaystart "2022-04-03", :holidayend "2022-04-03"}
+
+    (r/inner-drt "2022-03-10", "20220403-20220405,all day,vacation")
+    {:times "all day", :reason  "vacation", :holidaystart "2022-04-03", :holidayend "2022-04-05"}
+
+    (r/inner-drt "2022-03-10", "0403-0405,all day,vacation")
+    {:times "all day", :reason  "vacation", :holidaystart "2022-04-03", :holidayend "2022-04-05"}
 
     (r/inner-drt "2022-03-10", "0403-0405,all day,vacation")
     {:times "all day", :reason  "vacation", :holidaystart "2022-04-03", :holidayend "2022-04-05"}
@@ -24,4 +33,4 @@
     (r/inner-drt "2022-04-03", "2pm~,soccer")
     {:times "2pm~", :reason  "soccer", :date "2022-04-03"}
 
-             ))
+    ))
