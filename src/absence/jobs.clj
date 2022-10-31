095a294180c5f7ed98a4df7aff3f2f359fe4a59e
Nicolas Modrzyk <hellonico@gmail.com>
2022-10-31 14:38:14 +0900

    Setup for honeybadger #127

--- src/absence/jobs.clj
+++ src/absence/jobs.clj
@@ -1,21 +1,29 @@
 (ns absence.jobs
   (:require
-    [chime.core :as chime ]
-    [chime.core-async :as ca ]
-    [clojure.core.async :as a]
-    [config.core :refer [env]]
-    ;[clj-http.client :as client]
-    )
+    [chime.core :as chime]
+    [config.core :refer [env]])
   (:import [java.time Instant Duration])
   )
 
 
 (defn honey-checks[]
-  (let [
-        times (-> (chime/periodic-seq (Instant/now) (Duration/ofMinutes (-> env :honey-b :checkin :interval))))
-        chimes (ca/chime-ch times)]
-    (a/<!! (a/go-loop []
-                    (when-let [time (a/<! chimes)]
-                    (let [url (-> env :honey-b :checkin :url)]
+  (chime/chime-at (-> (chime/periodic-seq (Instant/now) (Duration/ofMinutes (-> env :honey-b :checkin :interval))))
+                  (fn [time]
+                    (let [url
+                          (-> env :honey-b :checkin :url)
+                          ]
                       (println "HoneyB Checkin" time " > " url)
-                      (slurp url)))))))
+                      (slurp url)))))
+
+
+
+
+;(defn honey-checks[]
+;  (let [
+;        times (-> (chime/periodic-seq (Instant/now) (Duration/ofMinutes (-> env :honey-b :checkin :interval))))
+;        chimes (ca/chime-ch times)]
+;    (a/<!! (a/go-loop []
+;                      (when-let [time (a/<! chimes)]
+;                        (let [url (-> env :honey-b :checkin :url)]
+;                          (println "HoneyB Checkin" time " > " url)
+;                          (slurp url)))))))
