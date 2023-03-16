ae1f23c1517d4e10c3306da0bc507d7defc051dc
Nicolas Modrzyk <hellonico@gmail.com>
2023-03-16 15:44:03 +0900

    upgrade 3rd party libraries versions

--- project.clj
+++ project.clj
@@ -9,25 +9,25 @@
                    ;:jvm-opts ["-DsocksProxyHost=localhost" "-DsocksProxyPort=10090" ]
                    :plugins [[com.jakemccrary/lein-test-refresh "0.25.0"]]}}
   :ring {:handler absence.ringing/handler :init absence.ringing/init}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [io.forward/clojure-mail "1.0.8"]
                  [com.draines/postal "2.0.5"]
-                 [org.clojure/core.async "1.5.648"]
+                 [org.clojure/core.async "1.6.673"]
 
                  [de.ubercode.clostache/clostache "1.4.0"]
                   [org.clojure/java.jdbc "0.7.12"]
                  ; [com.github.seancorfield/next.jdbc "1.2.772"]
                  ; https://cljdoc.org/d/com.github.seancorfield/next.jdbc/1.2.772/doc/getting-started
-                 [org.xerial/sqlite-jdbc "3.36.0.3"]
+                 [org.xerial/sqlite-jdbc "3.41.0.0"]
 
-                 [ring/ring "1.9.5"]
+                 [ring/ring "1.9.6"]
                  [ring-json-response "0.2.0"]
                  [ring-logger "1.1.1"]
-                 [compojure "1.6.2"]
+                 [compojure "1.7.0"]
                  [yogthos/config "1.2.0"]
 
-                 [org.clojars.mjdowney/excel-clj "2.1.0"]
-                 [org.apache.logging.log4j/log4j-core "2.17.2"]
+                 [org.clojars.mjdowney/excel-clj "2.2.0"]
+                 [org.apache.logging.log4j/log4j-core "2.20.0"]
 
                  [com.github.holidayjp/holidayjp-jdk8 "2.0.1"]
                  [org.clojure/data.json "2.4.0"]
@@ -36,7 +36,7 @@
                  [org.clojars.pntblnk/clj-ldap "0.0.17"]
 
                  ; NEW FEATURES
-                 [origami/origami "4.5.1-0"]
+                 [origami/origami "4.5.1-7"]
                  ;[io.sentry/sentry-clj "5.7.178"]
                  [camdez/honeybadger "0.4.1"]
 
